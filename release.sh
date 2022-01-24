83fa2f524549628fff619ee98bca93dc74e50577
Saluki <saluki@gmx.fr>
2022-01-24 06:44:15 +0100

    Adding Debian package builds

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 # This script helps with the release process on Github (musl & glibc builds for Linux)
 
 mkdir -p ./builds
-rm ./builds/*
+rm -rf ./builds/*
 
 CLI_VERSION=$(/usr/bin/cat Cargo.toml | egrep "version = (.*)" | egrep -o --color=never "([0-9]+\.?){3}" | head -n 1)
 echo "Releasing v$CLI_VERSION for GNU & musl targets"
@@ -18,6 +18,17 @@ cp -p ./target/x86_64-unknown-linux-musl/release/arp-scan ./builds/arp-scan-v$CL
 cargo build --release --target=x86_64-unknown-linux-gnu --locked
 cp -p ./target/x86_64-unknown-linux-gnu/release/arp-scan ./builds/arp-scan-v$CLI_VERSION-x86_64-unknown-linux-glibc
 ./builds/arp-scan-v$CLI_VERSION-x86_64-unknown-linux-glibc --version
+
+# Build the deb archive
+mkdir -p ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN
+echo "Package: arp-scan-rs" > ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN/control
+echo "Version: 0.13.0" >> ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN/control
+echo "Architecture: all" >> ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN/control
+echo "Maintainer: Saluki" >> ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN/control
+echo "Description: Minimalist ARP scan written in Rust" >> ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/DEBIAN/control
+mkdir -p ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/usr/local/bin
+cp ./builds/arp-scan-v$CLI_VERSION-x86_64-unknown-linux-glibc ./builds/arp-scan-rs_$CLI_VERSION-1_amd64/usr/local/bin/arp-scan
+(cd ./builds && dpkg-deb --build --root-owner-group arp-scan-rs_0.13.0-1_amd64)
 
 echo "Update the README instructions for v$CLI_VERSION"
 echo " ✓ Publish on crates.io"
